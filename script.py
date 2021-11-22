127eaccfa72480bf97816b38e42b054d973f41eb
Yaamiin <91520140+Yaamiin@users.noreply.github.com>
2021-11-22 14:04:35 +0300

    Update script.py

--- script.py
+++ script.py
@@ -14,11 +14,11 @@ For more click help....</b>"""
 <code>🌀 Your Edited Image will be Uploaded </code>
 
 
-© @TroJanzHEX"""
+© @Somalibots"""
     ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Image-Editor'>Click Here</a>"""
+⭕️<b>Channel :</b> 👉 <a href='https://t.me/Somalibots'>Click Here</a>"""
